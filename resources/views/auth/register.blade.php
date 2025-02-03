d0ec67da6891cfc728817147222dc20475711f6b
hommer <hommerlombrino991@gmail.com>
2025-02-03 14:17:11 +0800

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Log in & Registration</title>
+  <link rel="stylesheet" href="../../../login-register-form/style.css">
+</head>
+<body>
+<a href="javascript:history.back()" class="back-button">Back</a>  <input type="checkbox" id="check">
+  <div class="container">
+    <div class="login form">
+      <header>Sign Up</header>
+      <form action="#">
+        <input type="text" placeholder="Enter your email">
+        <input type="password" placeholder="Create a password">
+        <input type="password" placeholder="Confirm your password">
+        <input type="button" class="button" value="Signup">
+      </form>
+      <div class="signup">
+        <span class="signup">Already have an account?
+          <label for="check">Login</label>
+        </span>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
